9cce82fbb42e55c36d9c07166f2af16582332063
Fran Tena <tarod.net@gmail.com>
2020-05-24 12:10:04 +0200

    first commit

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    UnityEngine.AI.NavMeshAgent nav;
+
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player").transform;
+        playerHealth = player.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent <EnemyHealth> ();
+        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+    }
+
+
+    void Update ()
+    {
+		// Move only if both player and enemy are alive
+        if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
+            nav.SetDestination (player.position);
+        }
+		// Switch off NavMeshAgent
+        else
+        {
+            nav.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public int damagePerShot = 20;
+    public float timeBetweenBullets = 0.15f;
+    public float range = 100f;
+
+
+    float timer;
+    Ray shootRay;
+    RaycastHit shootHit;
+    int shootableMask;
+    ParticleSystem gunParticles;
+    LineRenderer gunLine;
+    AudioSource gunAudio;
+    Light gunLight;
+    float effectsDisplayTime = 0.2f;
+
+
+    void Awake ()
+    {
+        shootableMask = LayerMask.GetMask ("Shootable");
+        gunParticles = GetComponent<ParticleSystem> ();
+        gunLine = GetComponent <LineRenderer> ();
+        gunAudio = GetComponent<AudioSource> ();
+        gunLight = GetComponent<Light> ();
+    }
+
+
+    void Update ()
+    {
+		/* Similar to EnemyAttack.cs */
+        timer += Time.deltaTime;
+
+		if(Input.GetButton ("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0)
+        {
+            Shoot ();
+        }
+
+        if(timer >= timeBetweenBullets * effectsDisplayTime)
+        {
+            DisableEffects ();
+        }
+    }
+
+
+    public void DisableEffects ()
+    {
+        gunLine.enabled = false;
+        gunLight.enabled = false;
+    }
+
+
+    void Shoot ()
+    {
+		// Reset time because we're firing now
+        timer = 0f;
+
+        gunAudio.Play ();
+
+        gunLight.enabled = true;
+
+        gunParticles.Stop ();
+        gunParticles.Play ();
+
+		// Enable LineRenderer
+        gunLine.enabled = true;
+		// 0 = first position of the line
+        gunLine.SetPosition (0, transform.position);
+
+        shootRay.origin = transform.position;
+		/*
+		 * transform.forward - shorthand for writing Vector3(0, 0, 1)
+		 * 
+		 * transform.forward - blue axis
+		 */
+        shootRay.direction = transform.forward;
+
+		//  shootHit: information about where the collider was hit
+        if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+        {
+            EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
+			/* We can also hit walls or legos, so we must check if enemyHealth has
+			 * a script.
+			 */
+            if(enemyHealth != null)
+            {
+                enemyHealth.TakeDamage (damagePerShot, shootHit.point);
+            }
+			// 1 is the second part of the line. And we stop the line at the enemy
+            gunLine.SetPosition (1, shootHit.point);
+        }
+        else
+        {
+            gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+        }
+    }
+}
